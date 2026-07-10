b993c3e2c90e7242230ee33d99b39229687949f4
AlessiaCotini <alessia.cotini@icloud.com>
2026-07-10 09:19:28 +0200

    Initial commit

---
+++ src/test/java/alessia/cotini/gestioneviaggiaziendali/GestioneviaggiaziendaliApplicationTests.java
@@ -0,0 +1,13 @@
+package alessia.cotini.gestioneviaggiaziendali;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioneviaggiaziendaliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
